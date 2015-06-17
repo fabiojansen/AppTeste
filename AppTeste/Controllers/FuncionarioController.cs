9504d1014b782ae1ca478f99a404c85f80f03b10
Fábio Jansen <contato@fabiojansen.com.br>
2015-06-16 23:46:49 -0300

    Exemplo busca contracheque

---
+++ AppTeste/Controllers/FuncionarioController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppTeste.Models;
+
+namespace AppTeste.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario
+        public ActionResult Index()
+        {
+
+            var funcionario = new Funcionario()
+            {
+                Nome = "Fábio Jansen",
+                FuncionarioId = 1,
+                Matricula = "000123",
+                
+                
+            };
+
+            funcionario.Contracheque = funcionario.Matricula;
+
+            TempData["FuncionarioContracheque"] = funcionario.Contracheque;
+            
+            return View(funcionario);
+        }
+
+        public ActionResult VerContracheque(string filename)
+        {
+
+            var fileStream = new FileStream("d:/contracheque/" + TempData["FuncionarioContracheque"] + ".pdf",
+                                 FileMode.Open,
+                                 FileAccess.Read
+                               );
+            var fsResult = new FileStreamResult(fileStream, "application/pdf");
+            TempData.Keep();
+            return fsResult;
+         }
+    }
+}
---
+++ AppTeste/Models/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppTeste.Models
+{
+    public class Funcionario
+    {
+        public int FuncionarioId { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public string Contracheque { get; set; }
+
+    }
+}
